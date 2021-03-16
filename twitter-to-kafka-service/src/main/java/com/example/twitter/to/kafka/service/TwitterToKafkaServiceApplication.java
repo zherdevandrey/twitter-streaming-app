1a23e67f3b93b5ac2b3d3522d49acb9c920bc3a1
zherdev <mr.zherdev1992@gmail.com>
2021-03-17 00:50:10 +0300

    Twitter mock created

--- twitter-to-kafka-service/src/main/java/com/example/twitter/to/kafka/service/TwitterToKafkaServiceApplication.java
+++ twitter-to-kafka-service/src/main/java/com/example/twitter/to/kafka/service/TwitterToKafkaServiceApplication.java
@@ -1,13 +1,26 @@
 package com.example.twitter.to.kafka.service;
 
+import com.example.twitter.to.kafka.service.runner.StreamRunner;
+
+import lombok.AllArgsConstructor;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-public class TwitterToKafkaServiceApplication {
+@AllArgsConstructor
+@ComponentScan(basePackages = {"com.example.*"})
+public class TwitterToKafkaServiceApplication implements CommandLineRunner {
+
+    private StreamRunner streamRunner;
 
     public static void main(String[] args) {
         SpringApplication.run(TwitterToKafkaServiceApplication.class, args);
     }
 
+    @Override
+    public void run(String... args) throws Exception {
+        streamRunner.start();
+    }
 }
--- twitter-to-kafka-service/src/main/java/com/example/twitter/to/kafka/service/runner/StreamRunner.java
+++ twitter-to-kafka-service/src/main/java/com/example/twitter/to/kafka/service/runner/StreamRunner.java
@@ -1,4 +1,9 @@
 package com.example.twitter.to.kafka.service.runner;
 
+import twitter4j.TwitterException;
+
 public interface StreamRunner {
+
+    void start() throws TwitterException;
+
 }
