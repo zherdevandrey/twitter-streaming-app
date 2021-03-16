4682af007bdfdec2025973377cbc2f43e74b6b52
zherdev <mr.zherdev1992@gmail.com>
2021-03-16 23:41:44 +0300

    init

---
+++ twitter-to-kafka-service/src/main/java/com/example/twitter/to/kafka/service/runner/StreamRunner.java
@@ -0,0 +1,4 @@
+package com.example.twitter.to.kafka.service.runner;
+
+public interface StreamRunner {
+}
---
+++ twitter-to-kafka-service/src/test/java/com/example/twitter/to/kafka/service/TwitterToKafkaServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.twitter.to.kafka.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+//@SpringBootTest
+class TwitterToKafkaServiceApplicationTests {
+
+    //@Test
+    void contextLoads() {
+    }
+
+}
